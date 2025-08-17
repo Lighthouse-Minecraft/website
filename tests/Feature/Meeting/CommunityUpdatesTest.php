1cf242173b59a421746f74b7c031ae33d9a1c2f9
Jon Zenor <JonZenor@gmail.com>
2025-08-17 09:34:41 -0600

    Outlined tasks for the Community Updates page

---
+++ tests/Feature/Meeting/CommunityUpdatesTest.php
@@ -0,0 +1,20 @@
+<?php
+
+// Show a link in the sidebar navigation
+
+// Traveler and above should see the updates navigation
+
+// Below traveler should not see the updates navigation
+
+// The Community Updates page loads
+
+// Traveler and above should have access to the page
+
+// Below traveler should not have access to the page
+
+// List meetings that have been finalized
+
+// Show the Community Updates from that meeting
+
+
+// When Livewire 4 releases, make this an infinite scrolling list
