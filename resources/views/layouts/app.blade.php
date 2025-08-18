2d65b094eeacc0539dc4fc8d25cad7e2e13bf1e7
Ghost <92780719+Ghostridr@users.noreply.github.com>
2025-08-18 01:24:32 -0500

    Adds Livewire styles and scripts to layout
    Enables Livewire component support on all pages by injecting required styles and scripts into the main layout.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name', 'Lighthouse') }}</title>
     <link rel="stylesheet" href="/css/app.css">
+    @livewireStyles
 </head>
 <body>
     <div class="container mx-auto p-4">
         {{ $slot }}
     </div>
+    @livewireScripts
 </body>
 </html>
