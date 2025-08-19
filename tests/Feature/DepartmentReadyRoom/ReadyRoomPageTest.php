ee557ff7e177fa887a26cedbd50d7b096a2c1e77
Jon Zenor <JonZenor@gmail.com>
2025-08-18 20:13:18 -0600

    Outlined the Ready Room features

---
+++ tests/Feature/DepartmentReadyRoom/ReadyRoomPageTest.php
@@ -0,0 +1,21 @@
+<?php
+
+// The Ready Room link shows up in the sidebar for all ranks
+
+// The Ready Room link does not show up for members
+
+// The Ready Room page loads
+
+// The Ready Room page is accessible by all ranks
+
+// The Ready Room page is not accessible by members
+
+// The list of departments is displayed as a tab list
+
+// Only Officers can view all of the departments
+
+// JrCrew and Crew Members can only view their department
+
+// The recent meeting notes are displayed as a list
+
+// The current task list is displayed
