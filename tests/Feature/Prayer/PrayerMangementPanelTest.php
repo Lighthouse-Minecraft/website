f696f26c15a22d1479909c99a0bcfbf8471eec0a
Jon Zenor <JonZenor@gmail.com>
2025-08-23 12:43:36 -0600

    Ref #105 Planned out the work

---
+++ tests/Feature/Prayer/PrayerMangementPanelTest.php
@@ -0,0 +1,25 @@
+<?php
+
+describe('Prayer Management Panel - Display', function () {
+    // There is a Prayer management tab
+
+    // The Prayer Management component is displayed
+
+})->todo(issue: 105, assignee: 'jonzenor');
+
+describe('Prayer Management Panel - Editing', function () {
+    // The component displays a list of months
+
+    // The months open a modal with list of days
+
+    // The days open a modal to edit that days info
+
+    // Saving the changes updates the database
+})->todo(issue: 105, assignee: 'jonzenor');
+
+describe('Prayer Management Panel - Permissions', function () {
+    // The Command and Chaplain departments can view the panel
+
+    // The other officer departments cannot view the panel
+
+})->todo(issue: 105, assignee: 'jonzenor');
