8fd973826e2af8b56329c601b5c5d3ba99527b51
Jon Zenor <JonZenor@gmail.com>
2025-08-24 08:40:27 -0600

    Cleaned up and organized the dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,13 +2,31 @@
     <livewire:dashboard.alert-in-progress-meeting />
     <livewire:dashboard.view-announcements />
 
-    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-        <livewire:dashboard.view-rules />
 
-        <livewire:dashboard.announcements-widget />
+    <div class="w-full bg-zinc-900 p-4 rounded-lg mb-6">
+        <div class="flex mb-4">
+            <flux:heading>Community</flux:heading>
+            <flux:spacer />
+            <livewire:dashboard.view-rules />
+        </div>
 
-        @can('manage-stowaway-users')
-            <livewire:dashboard.stowaway-users-widget />
-        @endcan
+        <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+            <livewire:dashboard.announcements-widget />
+        </div>
     </div>
+
+    @if (auth()->user()->isAtLeastRank(App\Enums\StaffRank::Officer) || auth()->user()->isInDepartment(App\Enums\StaffDepartment::Quartermaster) || auth()->user()->isAdmin())
+        <div class="w-full bg-zinc-900 p-4 rounded-lg mb-6">
+            <div class="flex mb-4">
+                <flux:heading>Quartermaster</flux:heading>
+                <flux:spacer />
+            </div>
+
+            <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+                @can('manage-stowaway-users')
+                    <livewire:dashboard.stowaway-users-widget />
+                @endcan
+            </div>
+        </div>
+    @endif
 </x-layouts.app>
