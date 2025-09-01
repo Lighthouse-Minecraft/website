0ccf68c91afe26717b01af6fe65a77c9c981a566
Jon Zenor <JonZenor@gmail.com>
2025-08-31 20:37:10 -0600

    Created the donation page

--- config/lighthouse.php
+++ config/lighthouse.php
@@ -10,4 +10,11 @@ return [
     'donation_current_month_name' => env('DONATION_CURRENT_MONTH_NAME', ''),
     'donation_last_month_amount' => env('DONATION_LAST_MONTH_AMOUNT', 0),
     'donation_last_month_name' => env('DONATION_LAST_MONTH_NAME', ''),
+
+    'stripe' => [
+        'donation_pricing_table_id' => env('STRIPE_DONATION_PRICING_TABLE_ID', ''),
+        'donation_pricing_table_key' => env('STRIPE_DONATION_PRICING_TABLE_KEY', ''),
+        'customer_portal_url' => env('STRIPE_CUSTOMER_PORTAL_URL', ''),
+        'one_time_donation_url' => env('STRIPE_ONE_TIME_DONATION_URL', ''),
+    ],
 ];
--- resources/views/donation/index.blade.php
+++ resources/views/donation/index.blade.php
@@ -4,8 +4,10 @@
     <flux:text>LighthouseMC runs off of support from the community and donors who believe in the mission. We would not be here without their support. Consider making a donation to help us continue our work.</flux:text>
 
 
-    <flux:heading class="mt-4">Community Support</flux:heading>
+    <flux:heading class="mt-6">Community Support</flux:heading>
     <flux:text>Here’s how the community is helping cover our monthly costs:</flux:text>
+    <flux:text variant="subtle" size="sm" class="my-1">(Amounts are currently updated at least every 2 weeks)</flux:text>
+
     <div class="flex w-full">
         @if(config('lighthouse.donation_current_month_name'))
             <flux:card class="overflow-hidden min-w-[12rem] my-6 w-full md:w-1/6 mx-2">
@@ -20,8 +22,23 @@
                 <flux:heading size="xl" class="mt-2 tabular-nums flex">${{ config('lighthouse.donation_last_month_amount') }} <flux:text class="text-lg mt-1 mx-4">/ ${{ config('lighthouse.donation_goal') }}</flux:text></flux:heading>
             </flux:card>
         @endif
+
+        @if(config('lighthouse.stripe.one_time_donation_url'))
+            <flux:card class="overflow-hidden min-w-[16rem] my-6 w-full md:w-1/4 mx-2">
+                <flux:text>One-Time Donation</flux:text>
+                <flux:heading size="xl" class="mt-2 tabular-nums flex">
+                    <flux:button href="{{  config('lighthouse.stripe.one_time_donation_url') }}" variant="primary" color="sky">Make a One-Time Donation</flux:button>
+                </flux:heading>
+            </flux:card>
+        @endif
     </div>
-    <flux:text variant="subtle" class="my-1">Amounts are currently updated weekly</flux:text>
+
+    <flux:heading class="mt-6 mb-3">Monthly Support</flux:heading>
+    <script async src="https://js.stripe.com/v3/pricing-table.js"></script>
+    <stripe-pricing-table
+        pricing-table-id="{{  config('lighthouse.stripe.donation_pricing_table_id') }}"
+        publishable-key="{{  config('lighthouse.stripe.donation_pricing_table_key') }}">
+    </stripe-pricing-table>
 
     <flux:callout icon="user-group" class="w-full md:w-1/2 my-6" color="cyan">
         <flux:callout.heading>LighthouseMC is built on community support!</flux:callout.heading>
@@ -29,5 +46,10 @@
             <p class="my-3">Every gift helps us keep Minecraft a safe, fun, and Christ-centered place for kids and teens. Thank you for being part of this mission!</p>
             <p class="my-3">All staff members are volunteers and do not receive any financial or other compensation for their time and efforts. <strong>100% of donations go straight into ministry expenses.</strong></p>
         </flux:callout.text>
+
+        <x-slot name="actions">
+            <flux:button href="{{ config('lighthouse.stripe.customer_portal_url') }}" size="sm">Manage Subscription</flux:button>
+        </x-slot>
     </flux:callout>
+
 </x-layouts.app>
